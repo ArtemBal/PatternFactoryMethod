0dad83972ebb2814244bfb87decf8077305223fc
Artem Balan <a.s.balan96@gmail.com>
2023-06-09 23:11:12 +0300

    Simple example of Factory method pattern

---
+++ src/example/factory/WindowsDialog.java
@@ -0,0 +1,11 @@
+package example.factory;
+
+import example.buttons.Button;
+import example.buttons.WindowsButton;
+
+public class WindowsDialog extends Dialog{
+    @Override
+    public Button createButton() {
+        return new WindowsButton();
+    }
+}
